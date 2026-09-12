f1ea2a370953ef44f1182acf2afe6e4e3d522bf0
sir bravin <mrbravin69@gmail.com>
2026-09-12 15:40:12 +0300

    Add files via upload

---
+++ Ibrahim/helper2.js
@@ -0,0 +1,36 @@
+
+// utils/contextManager.js
+const NEWS_LETTER_JID = "120363419969992177@newsletter";
+const BOT_NAME = "jëëpërs çrëëpër ke";
+const thumbnails = [
+                " https://files.catbox.moe/wu8r4k.jpeg",
+                "https://files.catbox.moe/nnul7w.jpeg "
+            ];
+
+            // Select a random thumbnail
+            const DEFAULT_THUMBNAIL = thumbnails[Math.floor(Math.random() * thumbnails.length)];
+
+const createContext2 = (userJid, options = {}) => ({
+    contextInfo: {
+        mentionedJid: [userJid],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: NEWS_LETTER_JID,
+            newsletterName: BOT_NAME,
+            serverMessageId: Math.floor(100000 + Math.random() * 900000)
+        },
+        externalAdReply: {
+            title: options.title || BOT_NAME,
+            body: options.body || "Premium WhatsApp Bot Solution",
+            thumbnailUrl: options.thumbnail || DEFAULT_THUMBNAIL,
+            mediaType: 1,
+            showAdAttribution: true,
+            renderLargerThumbnail: true 
+        }
+    }
+});
+
+module.exports = {
+    createContext2
+};
